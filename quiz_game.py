c3529ead95bee258f7cea09c9a2f297e9f7937c4
Rayan84 <rayan_84@live.com>
2023-03-22 21:09:29 +0100

    Add uppercase tolerance functionality

--- quiz_game.py
+++ quiz_game.py
@@ -2,25 +2,25 @@ print("Welcome to my computer quiz!")
 
 playing = input("Do you want to play? ")
 
-if playing != "yes":
+if playing.lower() != "yes":
     quit()
 print("Ok! Let's play :) ")
 
 
 answer = input("What does CPU stand for?")
-if answer == "central processing unit":
+if answer.lower() == "central processing unit":
     print("Correct!")
-else: 
+else:
     print("Incorrect!")
 
 answer = input("What does GPU stand for?")
-if answer == "graphics processing unit":
+if answer.lower() == "graphics processing unit":
     print('Correct!')
-else: 
+else:
     print("Incorrect!")
 
 answer = input("What does RAM stand for?")
-if answer == "random access memory":
+if answer.lower() == "random access memory":
     print('Correct!')
-else: 
+else:
     print("Incorrect!")
